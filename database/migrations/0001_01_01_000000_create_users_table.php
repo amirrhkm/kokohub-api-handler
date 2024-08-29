d2fd2b02616dac115e7e5960b36bd3c3e8e8f6ec
asyraf12344543 <nikasyraf2001@gmail.com>
2024-08-30 00:57:36 +0800

    fix: remove foreign key constraint and change achievement_id column type
    - Removed foreign key constraint from achievement_id column in users table.
    - Changed achievement_id column data type to unsignedBigInteger and set it as nullable.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('user_id')->primary();
-            // $table->foreignIdFor(Achievement::class)->constrained()->cascadeOnDelete();
-            // $table->foreignIdFor(Achievement::class)->constrained()->cascadeOnDelete();
-            $table->string('achievement_id')->nullable();
-            $table->foreign('achievement_id')->references('achievement_id')->on('achievements')->onDelete('cascade');
-            // $table->foreignId('achievement_id')->references('achievement_id')->on('achievements')->onDelete('cascade');
+            $table->unsignedBigInteger('achievement_id')->nullable();
             $table->string('username');
             $table->string('password');
             $table->rememberToken();
